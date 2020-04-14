89d9b31658a10ad5d4d2d91202f872814348b01d
MikhailSadovsky <mishachess@mail.ru>
2020-04-14 19:12:58 +0300

    feat(kb): Remove custom-kb

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 PORT_NEW="8090"
 PORT_OLD="8000"
-IMAGE="ostis"
+IMAGE="ostis/ostis"
 VERSION="0.6.0"
 
 # Container paths
@@ -93,7 +93,7 @@ do
 done
 
 docker run -t -i \
-  -v ${KB_PATH}:${OSTIS_PATH}/custom-kb \
+  -v ${KB_PATH}:${OSTIS_PATH}/kb \
   -v ${PROBLEM_SOLVER_PATH}:${OSTIS_PATH}/problem-solver \
   -p ${PORT_NEW}:8090 \
   -p ${PORT_OLD}:8000 \
