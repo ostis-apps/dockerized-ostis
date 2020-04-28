46935803b2b3b481438526892a2e971c8cb6ad51
PlagaMedicum <katoshrodingera@protonmail.ch>
2020-04-29 01:19:24 +0300

    Improve start_container script

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
-cd ../sc-machine/scripts
-
+cd /ostis/sc-machine/scripts
 ./make_all.sh
 
-cd ../../scripts
-
+cd /ostis/scripts
 redis-server &
 ./build_kb.sh
 
 ./run_sctp.sh &
-
 echo "\n\e[1;32mStarting sc-web on http://localhost:8000...\e[0m\n"
-
 ./run_scweb.sh
 
