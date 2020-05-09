11f353b7c202680e3685efea01ce3eabf3a97b17
PlagaMedicum <katoshrodingera@protonmail.ch>
2020-05-09 15:58:34 +0300

    Fix build clion debug image script

--- clion_debug/build_clion_debug_image.sh
+++ clion_debug/build_clion_debug_image.sh
@@ -3,7 +3,7 @@
 IMAGE="clion/remote-cpp-env"
 VERSION="0.5"
 
-docker build -t ${IMAGE}:${VERSION} -f Dockerfile.clion-cpp-env .
+docker build -t ${IMAGE}:${VERSION} -f ./clion_debug/Dockerfile.clion-cpp-env .
 
 exit
 
