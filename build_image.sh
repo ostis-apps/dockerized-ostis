95c9cc50d7e3a8db115245c1e82f262edb4a8fb8
VasKho <vasya.khoroshavin@gmail.com>
2022-02-20 11:54:35 +0300

    Delete unnecessary scripts

--- build_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-IMAGE="ostis/ostis"
-VERSION="0.6.0"
-
-docker build -t ${IMAGE}:${VERSION} .
-
-exit
-
--- windows_scripts/build_image.ps1
+++
@@ -1,6 +0,0 @@
-$IMAGE = "ostis/ostis"
-$VERSION = "0.6.0"
-
-docker build -t ${IMAGE}:${VERSION} -f Dockerfile .
-
-exit
